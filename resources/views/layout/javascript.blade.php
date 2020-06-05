beb8841f4a9b6bc8a11d937f80282e56f69e5673
Triesutrisno <triesutrisno@gmail.com>
2020-06-06 00:24:24 +0700

    Portalsid #2020-06-05 00:18

---
+++ resources/views/layout/javascript.blade.php
@@ -0,0 +1,17 @@
+<!-- basic scripts -->
+
+	<!--[if !IE]> -->
+	<script src="{{ asset('assets/js/jquery-2.1.4.min.js') }}"></script>
+
+	<!-- <![endif]-->
+
+	<!--[if IE]>
+<script src="assets/js/jquery-1.11.3.min.js"></script>
+<![endif]-->
+	<script type="text/javascript">
+		if ('ontouchstart' in document.documentElement) document.write("<script src='assets/js/jquery.mobile.custom.min.js'>" + "<" + "/script>");
+	</script>
+	<script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+	<script src="{{ asset('assets/js/ace-elements.min.js') }}"></script>
+	<script src="{{ asset('assets/js/ace.min.js') }}"></script>
+	
