c53a841b45c2eada503bc2461666fe2c980bbc19
Triesutrisno <triesutrisno@gmail.com>
2020-06-22 11:48:20 +0700

    PortalSID #2020-06-22 11:48

--- app/Http/Controllers/Penjualan/JualDetailController_Aries.php
+++ app/Http/Controllers/Penjualan/JualDetailController_Aries.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use DB;
 
-class JualDetailController extends Controller
+class JualDetailController_Aris extends Controller
 {
     /**
      * Display a listing of the resource.
